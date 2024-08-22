a863b77f1c30f896128a759bce4c62335faca711
NetoTest <vikavasilenko2013@gmail.com>
2024-08-22 11:46:58 +0300

    Initial commit

---
+++ src/main/java/ru/netology/javaqa/Post.java
@@ -0,0 +1,14 @@
+package ru.netology.javaqa;
+
+import ru.netology.javaqa.FormDate;
+
+public class Post {
+    public String name;
+    public FormDate birthday;
+    public String passport;
+    public String patronymic;
+    public String phone;
+    public String surname;
+    public boolean subscription;
+
+}
